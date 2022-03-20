963d32f78e74bcad26c982f0da53ca9a5dfc0af1
Matheus Almeida <1519matheus@gmail.com>
2022-03-20 20:25:09 -0300

    Default Project: try 2

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 const express = require('express')
 const cors = require('cors')
-const path = require('path')
-const fs = require('fs')
 
 const routes = require('./routes')
 
 const app = express();
-// const server = require('http').Server(app);
-
-const https = require('https')
-const sslServer = https.createServer({
- key: fs.readFileSync(path.join(__dirname, '..', 'cert','key.pem')),
- cert: fs.readFileSync(path.join( __dirname,'..', 'cert','cert.pem')),
-}, app)
-
-const io = require('socket.io')(sslServer, { 
+const server = require('http').Server(app);
+const io = require('socket.io')(server, { 
   cors: {
     origin: "*",
     methods: ["GET", "POST"]
@@ -42,4 +33,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-sslServer.listen(process.env.PORT || 3443);
+server.listen(process.env.PORT || 3333);
